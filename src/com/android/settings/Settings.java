9f664393750eee0d28fe9d2f66d016130ed62113
beanstown106 <nbenis106@gmail.com>
2016-03-29 22:37:29 -0400

    Build PureSettings into settings [2/2]
    Change-Id: I0be173403ec4096e692c274bc42da2392257119e

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -160,4 +160,7 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+
+    // PureNexus Additions.
+    public static class PureNexusSettingsActivity extends SettingsActivity { /* empty */ }
 }
